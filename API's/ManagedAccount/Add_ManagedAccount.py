7f3e6a244af25019c8cae8a4c84619a2af3a22ef
Wesley Vieira <92549651+VieiraSantosz@users.noreply.github.com>
2024-01-10 09:44:04 -0300

    Update Add_ManagedAccount.py

--- API's/ManagedAccount/Add_ManagedAccount.py
+++ API's/ManagedAccount/Add_ManagedAccount.py
@@ -64,7 +64,7 @@ def Add_ManagedAccount_by_ManagedSystemID():
     
     print("Adicionar Managed Account\n")
     
-    with open(r'C:\Users\wsantos\Documents\APIs - Netconn\ManagedAccount\.SystemID.csv') as csvfile:
+    with open(r'Caminhho do arquivo csv') as csvfile:
         
         reader = csv.DictReader(csvfile)
         
