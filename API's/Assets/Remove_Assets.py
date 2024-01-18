edb0162eae577870e0bca1e470c5f6d13fc96dd9
Wesley Vieira <92549651+VieiraSantosz@users.noreply.github.com>
2024-01-18 11:01:12 -0300

    Update Remove_Assets.py

--- API's/Assets/Remove_Assets.py
+++ API's/Assets/Remove_Assets.py
@@ -15,16 +15,16 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 ### Configuração Cofre ###
-ipCofre       = '192.168.10.10'
-urlCofre      = f'https://{ipCofre}/BeyondTrust/api/public/v3'
-workgroupName = "BeyondTrust Workgroup"
+ip_cofre       = 'ip do cofre'
+url_cofre      = f'https://{ip_cofre}/BeyondTrust/api/public/v3'
+workgroupName  = "BeyondTrust Workgroup"
 ##########################
 
 
 ### Configuração API ###
-chaveApi = 'xxxxx'
-user     = 'user'
-headers  = {'Authorization': f'PS-Auth key={chaveApi};' f'runas={user};'}
+chave_api = 'xxxxx'
+user      = 'user'
+headers   = {'Authorization': f'PS-Auth key={chave_api};' f'runas={user};'}
 
 datype  = {'Content-type': 'application/json'}
 proxy   = {'http': None,'https': None}
@@ -43,17 +43,17 @@ session.headers.update(headers)
 ################# LogIn #################################
 def PostLogIn():
     
-    login = session.post(url = f'{urlCofre}/Auth/SignAppin', verify=False) 
+    login = session.post(url = f'{url_cofre}/Auth/SignAppin', verify = False) 
     
-    infoLogin = login.json()
+    info_login = login.json()
     
-    userId      = infoLogin['UserId']
-    userName    = infoLogin['UserName']
-    name        = infoLogin['Name']
+    userid      = info_login['UserId']
+    username    = info_login['UserName']
+    name        = info_login['Name']
     
     print("\nLogin Feito com Sucesso! - Codigo =", login.status_code)
-    print("\nUserId..:", userId, 
-          "\nUserName:", userName, 
+    print("\nUserId..:", userid, 
+          "\nUserName:", username, 
           "\nName....:", name)
     print()
 #########################################################
@@ -64,38 +64,38 @@ def Remove_Asset_by_id():
     
     print("Remover Assets!\n")
     
-    with open(r'Caminhho do arquivo csv') as csvfile:
+    with open(r'Caminho do arquivo csv') as csvfile:
         
         reader = csv.DictReader(csvfile)
         
         for row in reader:
-            AssetID = row['AssetID']
+            asset_id = row['AssetID']
             
-            urlGet      = urlCofre + f"/Assets/{AssetID}"
-            get_asset   = session.get(url = urlGet, verify=False)
+            url_get_asset   = url_cofre + f"/Assets/{asset_id}"
+            get_asset       = session.get(url = url_get_asset, verify=False)
             
-            infoAsset = get_asset.json()
+            info_asset = get_asset.json()
             get_asset.raise_for_status()
             
-            AssetName = infoAsset['AssetName'] 
+            asset_name = info_asset['AssetName'] 
             
-            urlAsset        = urlCofre + f"/Assets/{AssetID}"
-            remove_asset    = session.delete(url = urlAsset, verify=False)
+            url_remove_asset    = url_cofre + f"/Assets/{asset_id}"
+            remove_asset        = session.delete(url = url_remove_asset, verify=False)
             
             remove_asset.raise_for_status()
 
             if (remove_asset.status_code < 399):
-                print(f"[+] {AssetName} removido com sucesso. | Status Code = {remove_asset.status_code}")
+                print(f"[+] {asset_name} removido com sucesso. | Status Code = {remove_asset.status_code}")
             
             else:
-                print(f"[-] {AssetName} não removido. Erro: {remove_asset.json()} | Status Code = {remove_asset.status_code}")
+                print(f"[-] {asset_name} não removido. Erro: {remove_asset.json()} | Status Code = {remove_asset.status_code}")
 ####################################################################
 
 
 ################# LogOff #################################
 def PostLogOff():
     
-    logoff = session.post(url = f'{urlCofre}/Auth/Signout', verify=False)  
+    logoff = session.post(url = f'{url_cofre}/Auth/Signout', verify=False)  
 
     print("\nUsuario acabou de sair da sessao! - Codigo =", logoff.status_code)
     print()
@@ -107,4 +107,5 @@ def main():
     Remove_Asset_by_id()
     PostLogOff()
     
-main()
+if __name__ == '__main__':
+    main()
