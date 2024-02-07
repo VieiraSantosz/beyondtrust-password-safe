41cc1c39daeab07dddb39bdbc993d9ef009cb297
Wesley Vieira <92549651+VieiraSantosz@users.noreply.github.com>
2024-02-07 15:50:42 -0300

    Update Get_AddressGroup.py

--- API's/AddressGroup/Get_AddressGroup.py
+++ API's/AddressGroup/Get_AddressGroup.py
@@ -1,23 +1,13 @@
 import requests
-import json
-import urllib3
-import ssl
-from time import sleep
-import csv
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import os
+import warnings
+warnings.filterwarnings('ignore', category=requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 
 ### Configuração Cofre ###
 ip_cofre       = 'ip do cofre'
 url_cofre      = f'https://{ip_cofre}/BeyondTrust/api/public/v3'
-workgroupName  = "BeyondTrust Workgroup"
+workgroupname  = "BeyondTrust Workgroup"
 ##########################
 
 
@@ -51,6 +41,7 @@ def PostLogIn():
     username    = info_login['UserName']
     name        = info_login['Name']
     
+    os.system('cls')
     print('\nLogin Feito com Sucesso! - Codigo =', login.status_code)
     print('\nUserId..:', userid, 
           '\nUserName:', username, 
@@ -73,14 +64,15 @@ def Get_AddressGroup():
             address_id   = row['AddressGroupID']
             address_name = row['Name']
             
-            print(f'AddressGroupID - {str(address_id).ljust(5)} | AddressGroupName - {address_name}')
+            print(f'[+] AddressGroupID - {address_id} | AddressGroupName - {address_name}')
             
         except: 
-            print(f'[-] Erro: {info_group}')
+            print(f'[-] Erro: {info_group} | Status Code = {adressgroup.status_code}')
         
         
     addressgroup_id = input('\nDigite o ID do Address Group: ')
     print()
+      
         
     url_address = f'{url_cofre}/Addressgroups/{addressgroup_id}/addresses'
     get_address = session.get(url = url_address, verify = False)
@@ -89,16 +81,17 @@ def Get_AddressGroup():
         
     for row in get_address.json():
         try:
+            address_id  = row['AddressID']
             nome_adress = row['Value']
             
-            with open ('Caminhho do arquivo csv', 'a') as file:
-                file.write('\n' + nome_adress)
+            with open ('caminho do arquivo csv', 'a') as file:
+                file.write(f'\n{address_id},{nome_adress}')
                 
                 print(f'[+] {nome_adress} retirado com sucesso | Status Code = {get_address.status_code}')
                 
         except: 
             print(f'[-] Erro: {info_address} | Status Code = {get_address.status_code}')
-            break
+            continue
 #################################################################################################
 
 
